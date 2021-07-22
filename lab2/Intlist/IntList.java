906faeef59ec48223801ff66d43e16b431ee9bca
Adrian Prayoga <adrian.prayoga@synpulse.com>
2021-07-22 20:39:44 +0800

    push Dcatenate

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,21 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null) {
+            A = B;
+            return A;
+        }
+        if (B == null) return A;
+
+        IntList C = A;
+
+        while (A.rest != null) {
+            A = A.rest;
+        }
+
+        A.rest = B;
+
+        return C;
     }
 
     /**
