ff61dfef682e8e9386d3e8695aab215f1be7e6d1
Adrian Prayoga <adrian.prayoga@synpulse.com>
2021-08-08 08:45:05 +0800

    Added changes for lab6

--- proj2/byog/lab6/MemoryGame.java
+++ proj2/byog/lab6/MemoryGame.java
@@ -1,5 +1,6 @@
 package byog.lab6;
 
+import byog.Core.RandomUtils;
 import edu.princeton.cs.introcs.StdDraw;
 
 import java.awt.Color;
@@ -19,14 +20,15 @@ public class MemoryGame {
                                                    "Too easy for you!", "Wow, so impressive!"};
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Please enter a seed");
-            return;
-        }
+//        if (args.length < 1) {
+//            System.out.println("Please enter a seed");
+//            return;
+//        }
 
-        int seed = Integer.parseInt(args[0]);
+        int seed = 1; //Integer.parseInt(args[0]);
+
         MemoryGame game = new MemoryGame(40, 40);
-        game.startGame();
+        game.startGame(seed);
     }
 
     public MemoryGame(int width, int height) {
@@ -42,33 +44,93 @@ public class MemoryGame {
         StdDraw.setYscale(0, this.height);
         StdDraw.clear(Color.BLACK);
         StdDraw.enableDoubleBuffering();
-
-        //TODO: Initialize random number generator
     }
 
-    public String generateRandomString(int n) {
-        //TODO: Generate random string of letters of length n
-        return null;
+    public String generateRandomString(int n, Random RANDOM) {
+        String randomString = "";
+        for(int i = 0; i < n; i++) {
+            randomString += CHARACTERS[RANDOM.nextInt(CHARACTERS.length)];
+        }
+
+        return randomString;
     }
 
     public void drawFrame(String s) {
-        //TODO: Take the string and display it in the center of the screen
-        //TODO: If game is not over, display relevant game information at the top of the screen
+        StdDraw.clear();
+        StdDraw.line(0, 38, 40, 38);
+        Font font = new Font("Monaco", Font.BOLD, 15);
+        StdDraw.setFont(font);
+        StdDraw.text(5, 39,"Round: ");
+        StdDraw.text(20, 39,"Watch!");
+        StdDraw.text(20, 20, s);
+        StdDraw.show();
+    }
+
+    public void drawUserFrame(String s) {
+        StdDraw.clear();
+        StdDraw.line(0, 38, 40, 38);
+        Font font = new Font("Monaco", Font.BOLD, 15);
+        StdDraw.setFont(font);
+        StdDraw.text(5, 39,"Round: ");
+        StdDraw.text(20, 39,"Type!");
+        StdDraw.text(20, 22, "TYPE YOUR ANSWER!");
+        StdDraw.text(20, 18, s);
+        StdDraw.show();
     }
 
     public void flashSequence(String letters) {
-        //TODO: Display each character in letters, making sure to blank the screen between letters
+        for (Character c : letters.toCharArray()) {
+            drawFrame("");
+            StdDraw.pause(500);
+            drawFrame(c.toString());
+            StdDraw.pause(1000);
+        }
     }
 
     public String solicitNCharsInput(int n) {
-        //TODO: Read n letters of player input
-        return null;
+        drawUserFrame("");
+
+        String s = "";
+        outer: while(s.length() < n) {
+            if(StdDraw.hasNextKeyTyped()) {
+                char c = StdDraw.nextKeyTyped();
+
+                if (c == ':') {
+                    break outer;
+                }
+
+                s += String.valueOf(c);
+                drawUserFrame(s);
+            }
+        }
+
+        return s;
     }
 
-    public void startGame() {
-        //TODO: Set any relevant variables before the game starts
+    public void startGame(int seed) {
+        Random RANDOM = new Random(seed);
 
-        //TODO: Establish Game loop
+        int numOfChar = 1;
+        while (numOfChar < 10) {
+            String randomString = generateRandomString(numOfChar, RANDOM);
+
+            flashSequence(randomString);
+            String userInput = solicitNCharsInput(numOfChar);
+
+            System.out.println("output "+ userInput);
+
+            if(!userInput.equals(randomString)) {
+                StdDraw.clear();
+                StdDraw.text(20, 24, "WRONG INPUT! CORRECT ANSWER IS");
+                StdDraw.text(20, 20, randomString);
+                StdDraw.text(20, 16, "YOU MADE IT TO ROUND " + numOfChar);
+                StdDraw.show();
+                break;
+            }
+
+            numOfChar++;
+        }
+
     }
 
 }
